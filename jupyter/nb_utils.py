6fe8ebbb07e335a284e5e3b98513883fa9fa9ebe
fscheidt <felippescheidt@gmail.com>
2022-01-29 13:06:46 -0300

    update nb_utils

--- jupyter/nb_utils.py
+++ jupyter/nb_utils.py
@@ -52,16 +52,28 @@ def get_notebook_rel_base_path(target_base='notebooks',
 
 
 NOTE_DIR = get_notebook_dir()
-print('Notebook location:', NOTE_DIR)
+print('\nThis_notebook_path:\n\t', NOTE_DIR)
 
 NOTEBOOKS_ABS_DIR = get_notebook_abs_base_path(resource_dir='')
-print('Notebooks base folder:', NOTEBOOKS_ABS_DIR)
+print('\nNotebooks_abs_path:\n\t', NOTEBOOKS_ABS_DIR)
 
-NOTEBOOKS_BASE_REL = get_notebook_rel_base_path()
-print('Notebooks relative folder:', NOTEBOOKS_BASE_REL)
+NOTEBOOKS_REL_DIR = get_notebook_rel_base_path(resource_dir='')
+print('\nNotebooks_rel_relative_path:\n\t', NOTEBOOKS_REL_DIR)
+
+PROJECT_ABS_DIR = get_notebook_abs_base_path(target_base="gitlog_builder",resource_dir='')
+print('\nProject_abs_path:\n\t', PROJECT_ABS_DIR)
+
+PROJECT_REL_DIR =  get_notebook_rel_base_path(target_base="gitlog_builder", resource_dir='')
+print('\nProject_rel_path:\n\t', PROJECT_REL_DIR)
+
+STATIC_DIR_REL_PATH = get_notebook_rel_base_path(resource_dir="static")
+print('\nStatic_dir_relative_path:\n\t', STATIC_DIR_REL_PATH)
+
+STATIC_DIR_ABS_PATH = get_notebook_abs_base_path(resource_dir="static")
+print('\nStatic_dir_absolute_path:\n\t', STATIC_DIR_ABS_PATH)
 
 RENDER_JS_ABS_PATH = get_notebook_abs_base_path(file_name="renderjson.js")
-print('renderjson.js abs path:', RENDER_JS_ABS_PATH)
+print('\n[renderjson.js] abs_path:\n\t', RENDER_JS_ABS_PATH)
 
-RENDER_JS_REL_PATH = get_notebook_rel_base_path(file_name="renderjson.js")
-print('renderjson.js rel path:', RENDER_JS_REL_PATH)
+RENDER_JS_REL_PATH = get_notebook_rel_base_path(resource_dir="static", file_name="renderjson.js")
+print('\n[renderjson.js] rel_path:\n\t', RENDER_JS_REL_PATH)
