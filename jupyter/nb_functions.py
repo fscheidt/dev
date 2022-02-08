27adcdb3fd298f42d862bdfa052b1d2d76f8c9ce
fscheidt <felippescheidt@gmail.com>
2022-02-08 12:04:00 -0300

    add nb_functions

---
+++ jupyter/nb_functions.py
@@ -0,0 +1,12 @@
+# update_version: 
+class NBFunctions:
+    
+    def __init__():
+        pass
+    
+    @staticmethod
+    def restart():
+        from IPython.core.display import HTML
+        return HTML("<script>Jupyter.notebook.kernel.restart()</script>")
+    
+nbf = NBFunctions
