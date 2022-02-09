3f0faf7ed7ff76ca4574b684f6f1cdb73ce3d0d7
fscheidt <felippescheidt@gmail.com>
2022-02-09 08:35:53 -0300

    update nb_functions

--- jupyter/nb_functions.py
+++ jupyter/nb_functions.py
@@ -3,6 +3,16 @@ from datetime import datetime
 from time import strftime
 from time import gmtime
 import time
+import uuid
+from IPython.display import display_javascript, display_html, display
+import json
+
+JS_FILE = None
+try:
+    JS_FILE = RENDER_JS_LC_PATH
+except:
+    JS_FILE = "https://raw.githubusercontent.com/fscheidt/dev/master/jupyter/renderjson.js"
+
 
 """
 Load this file into a notebook:
@@ -37,9 +47,57 @@ class RuntimeLogger:
     def runtime(duration):
         print(f'Total run_time: {strftime("%H:%M:%S", gmtime(duration))}')
 
+
+class RenderJSON(object):
+    def __init__(self, json_data):
+        json_data = RenderJSON.doc_to_json(json_data)
+        if isinstance(json_data, dict):
+            self.json_str = json.dumps(json_data)
+        else:
+            self.json_str = json_data
+        self.uuid = str(uuid.uuid4())
+
+    def _ipython_display_(self):
+        display_html('<div class="render_json" id="{}" style="font-size:1.2em; height: 600px; width:100%;"></div>'.format(self.uuid), raw=True)
+        display_javascript("""
+        require(['"""+JS_FILE+"""'], function() {
+        document.getElementById('%s').appendChild(renderjson(%s))
+        });
+        """ % (self.uuid, self.json_str), raw=True)
+
+    @staticmethod
+    def pprint(json_data):
+        """ print using pygments """
+        json_data = RenderJSON.doc_to_json(json_data)
+        from pygments import highlight, lexers, formatters
+        formatted_json = json.dumps(json_data, sort_keys=True, indent=4)
+        colorful_json = highlight(formatted_json, lexers.JsonLexer(), formatters.TerminalFormatter())
+        print(colorful_json)
+    
+    @staticmethod
+    def dprint(data, indent=4, sort_keys=True):
+        data = RenderJSON.doc_to_json(data)
+        print(json.dumps(data, indent=indent, sort_keys=sort_keys))
+
+    @staticmethod
+    def doc_to_json(mongo_doc):
+        """
+        convert mongo document to json
+        """        
+        import importlib
+        bson_installed = importlib.util.find_spec("bson")
+        found = bson_installed is not None
+        if found:
+            from bson import json_util
+        else:
+            return mongo_doc
+        return json.loads(json_util.dumps(mongo_doc))
+
+
 class NBFunctions:
     
     log = RuntimeLogger(v=True)
+    render = RenderJSON
     
     def __init__(self, v=True):
         self.v = v
