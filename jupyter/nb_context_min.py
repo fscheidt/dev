d41947461f3c81ae4474dc328bdb9bc9b569f829
fscheidt <felippescheidt@gmail.com>
2022-03-14 11:56:20 -0300

    update

--- jupyter/nb_context_min.py
+++ jupyter/nb_context_min.py
@@ -1,13 +1,13 @@
-# brief context setup
+# set context and import common dependencies
+from IPython.display import display_javascript, display_html, display, HTML
 import os
-"""
-Load into notebook:
-%load https://raw.githubusercontent.com/fscheidt/dev/master/jupyter/nb_context_min.py
-"""
-
+from datetime import datetime
+from time import strftime
 DIR_PATH = os.getenv('GTB_DEV')
 %cd $DIR_PATH
 !conda info | grep 'active envir'
+
+print(f"[load_time]: {datetime.now().strftime('%Y/%m/%d - %H:%M:%S')}")
 
 from IPython.lib.deepreload import reload
 %load_ext autoreload
