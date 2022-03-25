c888c8770d692c17a04780783e48305be80c84b9
fscheidt <felippescheidt@gmail.com>
2022-03-25 10:49:41 -0300

    update render

--- jupyter/nb_render_json.py
+++ jupyter/nb_render_json.py
@@ -1,10 +1,8 @@
-# source: https://github.com/fscheidt/dev/blob/master/jupyter/nb_render_json.py
-from IPython.display import display_javascript, display_html, display, HTML
+# RENDER JSON
+from IPython.display import display_javascript, display_html
 import uuid
 import json
-
 JS_FILE = "https://raw.githubusercontent.com/fscheidt/dev/master/jupyter/renderjson.js",
-
 class RenderJSON(object):
     def __init__(self, json_data):
         json_data = RenderJSON.doc_to_json(json_data)
@@ -13,7 +11,6 @@ class RenderJSON(object):
         else:
             self.json_str = json_data
         self.uuid = str(uuid.uuid4())
-
     def _ipython_display_(self):
         display_html('<div class="render_json" id="{}" style="font-size:1.2em; height: 600px; width:100%;"></div>'.format(self.uuid), raw=True)
         display_javascript("""
@@ -21,20 +18,17 @@ class RenderJSON(object):
         document.getElementById('%s').appendChild(renderjson(%s))
         });
         """ % (self.uuid, self.json_str), raw=True)
-
     @staticmethod
     def pprint(json_data, sort_keys=True, indent=4):
         json_data = RenderJSON.doc_to_json(json_data)
         from pygments import highlight, lexers, formatters
         formatted_json = json.dumps(json_data, sort_keys=sort_keys, indent=indent)
         colorful_json = highlight(formatted_json, lexers.JsonLexer(), formatters.TerminalFormatter())
-        print(colorful_json)
-    
+        print(colorful_json)   
     @staticmethod
     def dprint(data, indent=4, sort_keys=True):
         data = RenderJSON.doc_to_json(data)
         print(json.dumps(data, indent=indent, sort_keys=sort_keys))
-
     @staticmethod
     def doc_to_json(mongo_doc) -> dict:      
         import importlib
@@ -44,23 +38,15 @@ class RenderJSON(object):
         else:
             return mongo_doc
         return json.loads(json_util.dumps(mongo_doc))
-
 class NBFunctions:    
     render = RenderJSON    
     @staticmethod
     def pprint(json_data, sort_keys=True, indent=4):
-        """ wrapper to render.pprint() """
         NBFunctions.render.pprint(json_data)
-    @staticmethod    
-    def dec(value):
-        return '{:,}'.format(value)
     @staticmethod
-    def f2(value):
-        return "{:.2f}".format(value)    
+    def dec(value):
+        return '{:,}'.format(value) 
     @staticmethod
     def f4(value):
         return "{:.4f}".format(value)
-
 nb = NBFunctions()
-# nb.dec(100000)
-# nb.pprint({'data': '1'})
